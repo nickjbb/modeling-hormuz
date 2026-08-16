e9bead10c4edf6caa7bfb3ac610275812bb6f7f1
nickjbb <nickjbattin@gmail.com>
2026-08-16 15:12:53 -0700

    Fixed Issue with macro functions; added to requirements.txt

--- modeling_hormuz/macros/statistics.sql
+++ modeling_hormuz/macros/statistics.sql
@@ -1,62 +1,62 @@
 {% macro daily_average(table_name, date_column, value_column) %}
-    select
+    (select
         avg({{ value_column }}) as daily_avg
     from {{ table_name }}
-    where date_trunc('day', {{ date_column }}) = date_trunc('day', current_date)
+    where date_trunc('day', {{ date_column }}) = date_trunc('day', current_date))
 {% endmacro %}
 
 {% macro weekly_average(table_name, date_column, value_column) %}
-    select
+    (select
         avg({{ value_column }}) as weekly_avg
     from {{ table_name }}
-    where date_trunc('week', {{ date_column }}) = date_trunc('week', current_date)
+    where date_trunc('week', {{ date_column }}) = date_trunc('week', current_date))
 {% endmacro %}
 
 {% macro monthly_average(table_name, date_column, value_column) %}
-    select
+    (select
         avg({{ value_column }}) as monthly_avg
     from {{ table_name }}
-    where date_trunc('month', {{ date_column }}) = date_trunc('month', current_date)
+    where date_trunc('month', {{ date_column }}) = date_trunc('month', current_date))
 {% endmacro %}
 
 {% macro daily_max(table_name, date_column, value_column) %}
-    select
+    (select
         max({{ value_column }}) as daily_max
     from {{ table_name }}
-    where date_trunc('day', {{ date_column }}) = date_trunc('day', current_date)
+    where date_trunc('day', {{ date_column }}) = date_trunc('day', current_date))
 {% endmacro %}
 
 {% macro weekly_max(table_name, date_column, value_column) %}
-    select
+    (select
         max({{ value_column }}) as weekly_max
     from {{ table_name }}
-    where date_trunc('week', {{ date_column }}) = date_trunc('week', current_date)
+    where date_trunc('week', {{ date_column }}) = date_trunc('week', current_date))
 {% endmacro %}
 
 {% macro monthly_max(table_name, date_column, value_column) %}
-    select
+    (select
         max({{ value_column }}) as monthly_max
     from {{ table_name }}
-    where date_trunc('month', {{ date_column }}) = date_trunc('month', current_date)
+    where date_trunc('month', {{ date_column }}) = date_trunc('month', current_date))
 {% endmacro %}
 
 {% macro daily_min(table_name, date_column, value_column) %}
-    select
+    (select
         min({{ value_column }}) as daily_min
     from {{ table_name }}
-    where date_trunc('day', {{ date_column }}) = date_trunc('day', current_date)
+    where date_trunc('day', {{ date_column }}) = date_trunc('day', current_date))
 {% endmacro %}
 
 {% macro weekly_min(table_name, date_column, value_column) %}
-    select
+    (select
         min({{ value_column }}) as weekly_min
     from {{ table_name }}
-    where date_trunc('week', {{ date_column }}) = date_trunc('week', current_date)
+    where date_trunc('week', {{ date_column }}) = date_trunc('week', current_date))
 {% endmacro %}
 
 {% macro monthly_min(table_name, date_column, value_column) %}
-    select
+    (select
         min({{ value_column }}) as monthly_min
     from {{ table_name }}
-    where date_trunc('month', {{ date_column }}) = date_trunc('month', current_date)
+    where date_trunc('month', {{ date_column }}) = date_trunc('month', current_date))
 {% endmacro %}
